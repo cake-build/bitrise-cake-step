9964532a8c5aeaaa39bd53a2bc68c4fa7090d5f7
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2017-04-22 19:47:21 +0200

    Brew update

--- step.sh
+++ step.sh
@@ -3,11 +3,11 @@
 if [ ${dotnetcore} == "yes" ]
 then
   echo "Installing .net core"
-  brew update
-  brew install openssl
-  mkdir -p /usr/local/lib
-  ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
-  ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
+  #brew update
+  #brew install openssl
+  #mkdir -p /usr/local/lib
+  #ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+  #ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
   #wget https://raw.githubusercontent.com/dotnet/cli/rel/1.0.1/scripts/obtain/dotnet-install.sh
   #chmod +x dotnet-install.sh
   #./dotnet-install.sh 
@@ -15,11 +15,13 @@ then
   brew tap caskroom/cask
   brew cask install dotnet
 
+  find $HOME -name "dotnet"
+
   $HOME/.dotnet/dotnet --version
   export PATH=/$HOME/.dotnet:$PATH
   dotnet --version
 
-  #find $HOME/.dotnet -name "dotnet"
+  
 
 fi
 
