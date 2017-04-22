b5327e9f86fad7416c1afde092a8b4884cf83ab7
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2017-04-22 18:20:07 +0200

    Add some cli params

--- step.sh
+++ step.sh
@@ -10,7 +10,7 @@ then
   ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
   wget https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.sh
   chmod +x dotnet-install.sh
-  ./dotnet-install.sh
+  ./dotnet-install.sh --install-dir $HOME --verbose
   ln -s /usr/local/share/dotnet/dotnet /usr/local/bin/
   dotnet --version
 fi
