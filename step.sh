cc49ae420e67319814560f11688c39cae59f580a
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2017-04-30 17:02:53 +0200

    Adding some fixes for .net core support

--- step.sh
+++ step.sh
@@ -4,6 +4,12 @@ if [ ${dotnetcore} == "yes" ]
 then
   echo "Installing .net core"
 
+  brew update
+  brew install openssl
+  mkdir -p /usr/local/lib
+  ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+  ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
+
   wget https://raw.githubusercontent.com/dotnet/cli/rel/1.0.1/scripts/obtain/dotnet-install.sh
   chmod +x dotnet-install.sh
   ./dotnet-install.sh 
