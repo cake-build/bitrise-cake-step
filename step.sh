0661398509a1185cac28f02386d4b28fa438edaf
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2018-03-08 23:05:44 +0100

    Fix bash command

--- step.sh
+++ step.sh
@@ -5,11 +5,11 @@ curl -Lsfo build.sh https://cakebuild.net/download/bootstrapper/osx
 
 nuget="/Library/Frameworks/Mono.framework/Versions/Current/bin/nuget"
 
-if [ -z "${nuget_source_name}" ] && -z "${nuget_source_path_or_url}" ] && ! -z "${nuget_source_username}" ] && [ ! -z "${nuget_source_password}" ] ; then
+if [ -z "${nuget_source_name}" ] && [ -z "${nuget_source_path_or_url}" ] && [ ! -z "${nuget_source_username}" ] && [ ! -z "${nuget_source_password}" ] ; then
   "${nuget}" sources add -Name "${nuget_source_name}" -Source "${nuget_source_path_or_url}" -UserName "${nuget_source_username}" -Password "${nuget_source_password}" -Verbosity "detailed"
 fi
 
-if [ -z "${nuget_source_name}" ] && -z "${nuget_source_path_or_url}" ] && -z "${nuget_source_username}" ] && [ -z "${nuget_source_password}" ] ; then
+if [ -z "${nuget_source_name}" ] && [ -z "${nuget_source_path_or_url}" ] && [ -z "${nuget_source_username}" ] && [ -z "${nuget_source_password}" ] ; then
   "${nuget}" sources add -Name "${nuget_source_name}" -Source "${nuget_source_path_or_url}" -Verbosity "detailed"
 fi
 
