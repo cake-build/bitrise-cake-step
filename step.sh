7fbdb53d83616071f54e58cec037cf95b224135b
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2018-03-08 23:40:36 +0100

    Debug

--- step.sh
+++ step.sh
@@ -3,14 +3,17 @@
 echo "Installing Cake bootstrapper"
 curl -Lsfo build.sh https://cakebuild.net/download/bootstrapper/osx
 
+echo "Checking custom nuget sources"
 nuget="/Library/Frameworks/Mono.framework/Versions/Current/bin/nuget"
 
 if [ -z "${nuget_source_name}" ] && [ -z "${nuget_source_path_or_url}" ] && [ ! -z "${nuget_source_username}" ] && [ ! -z "${nuget_source_password}" ] ; then
-  "${nuget}" sources add -Name "${nuget_source_name}" -Source "${nuget_source_path_or_url}" -UserName "${nuget_source_username}" -Password "${nuget_source_password}" -Verbosity "detailed"
+    echo "${nuget_source_name}"
+    "${nuget}" sources add -Name "${nuget_source_name}" -Source "${nuget_source_path_or_url}" -UserName "${nuget_source_username}" -Password "${nuget_source_password}" -Verbosity "detailed"
 fi
 
 if [ -z "${nuget_source_name}" ] && [ -z "${nuget_source_path_or_url}" ] && [ -z "${nuget_source_username}" ] && [ -z "${nuget_source_password}" ] ; then
-  "${nuget}" sources add -Name "${nuget_source_name}" -Source "${nuget_source_path_or_url}" -Verbosity "detailed"
+    echo "${nuget_source_name}"
+    "${nuget}" sources add -Name "${nuget_source_name}" -Source "${nuget_source_path_or_url}" -Verbosity "detailed"
 fi
 
 echo "Executing Cake Script"
